966c579b6c0ac851cc8b8beb532c9cb072646213
Łukasz <riraseth@gmail.com>
2020-03-14 21:27:31 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import styles from '../css/footer.module.css';
+import links from '../constants/links';
+import socialIcons from '../constants/social-icons';
+import { Link } from 'gatsby';
 const Footer = () => {
   return (
-    <div>
-      <h1>hello from footer</h1>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((link, index) => {
+          return (
+            <Link key={index} to={link.path}>
+              {link.text}
+            </Link>
+          );
+        })}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((icon, index) => {
+          return (
+            <a
+              key={index}
+              href={icon.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {icon.icon}
+            </a>
+          );
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &copy; backroads travel company {new Date().getFullYear()} all
+        rights reserved
+      </div>
+    </footer>
   );
 };
 
