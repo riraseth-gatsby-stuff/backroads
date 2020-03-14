8b2fd7e78163f5e9a9f88253dbdeba401ed83c18
Łukasz <riraseth@gmail.com>
2020-03-14 21:52:24 +0100

    hero, banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import SimpleHero from '../components/SimpleHero';
+import Banner from '../components/Banner';
 export default () => (
   <Layout>
-    <div>Hello world!</div>
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, voluptatibus!"
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 );
