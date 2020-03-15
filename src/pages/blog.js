f53a379b765e85c2bf26c0beb9b78bab2b1cc545
Łukasz <riraseth@gmail.com>
2020-03-15 21:31:19 +0100

    images setup

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
-import { graphql } from 'gatsby';
-const blog = props => {
-  console.log(props);
+const blog = () => {
   return (
     <Layout>
       hello from blog page
       <Link to="/">Home</Link>
-      <h1>title: {props.data.site.siteMetadata.title}</h1>
     </Layout>
   );
 };
-
-// works only in pages folder
-export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-        description
-        author
-      }
-    }
-  }
-`;
 
 export default blog;
