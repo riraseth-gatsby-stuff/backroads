de4d48664c911f0a4ea7b0505dc8b108e97ac217
Łukasz <riraseth@gmail.com>
2020-03-15 19:43:33 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,11 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: 'BackRoads',
+    description:
+      'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+    author: '@Łukasz Zabuski'
+  },
   plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`]
 };
