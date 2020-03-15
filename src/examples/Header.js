15e1fe08dd1e296bb3feb5c13b9fec881f57b345
Łukasz <riraseth@gmail.com>
2020-03-15 19:59:09 +0100

    Update Header.js

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+
+const getSiteData = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`;
+
 const Header = () => {
   const {
     site: {
       siteMetadata: { title, author }
     }
-  } = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          title
-          description
-          author
-        }
-      }
-    }
-  `);
+  } = useStaticQuery(getSiteData);
   return (
     <div>
       <h1>title: {title}</h1>
