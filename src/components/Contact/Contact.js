2fac86d52e702e147a373b48cd9751ac2e791856
Łukasz <riraseth@gmail.com>
2020-03-17 22:10:42 +0100

    contact form, formspree

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Title from '../StyledTitle';
+import styles from '../../css/contact.module.css';
+const Contact = () => {
+  return (
+    <section className={styles.contact}>
+      <Title title="contact" subtitle="us"></Title>
+      <div className={styles.center}>
+        <form
+          action="https://formspree.io/xeqzkren"
+          method="POST"
+          className={styles.form}
+        >
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className={styles.formControl}
+              placeholder="john doe"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className={styles.formControl}
+              placeholder="email@email.com"
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <textarea
+              name="message"
+              id="message"
+              rows="10"
+              className={styles.formControl}
+              placeholder="hello"
+            ></textarea>
+          </div>
+          <div>
+            <input type="submit" value="submit" className={styles.submit} />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
