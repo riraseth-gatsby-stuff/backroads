28300b47626915a8e39080eda8ab4d43a5f79882
Łukasz <riraseth@gmail.com>
2020-03-17 21:01:31 +0100

    styled hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/Layout';
-import SimpleHero from '../components/SimpleHero';
 import Banner from '../components/Banner';
 import About from '../components/Home/About';
 import Services from '../components/Home/Services';
-export default () => (
-  <Layout>
-    <SimpleHero>
-      <Banner
-        title="continue exploring"
-        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, voluptatibus!"
-      >
-        <Link to="/tours" className="btn-white">
-          explore tours
-        </Link>
-      </Banner>
-    </SimpleHero>
-    <About></About>
-    <Services></Services>
-  </Layout>
-);
+import StyledHero from '../components/StyledHero';
+
+//with export we can skip useStaticQuery
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 4160, quality: 90) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
+
+export default ({ data }) => {
+  // const data = useStaticQuery(query);
+  return (
+    <Layout>
+      <StyledHero home={true} img={data.defaultBcg.childImageSharp.fluid}>
+        <Banner
+          title="continue exploring"
+          info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, voluptatibus!"
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </StyledHero>
+      <About></About>
+      <Services></Services>
+    </Layout>
+  );
+};
