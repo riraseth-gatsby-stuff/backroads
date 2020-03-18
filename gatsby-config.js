51ce45fdeb99fe6bd9459e17ff56ee9c534a1aa6
Łukasz <riraseth@gmail.com>
2020-03-18 20:52:18 +0100

    env

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  siteMetadata: {
+    title: 'BackRoads',
+    description:
+      'Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure',
+    author: '@Łukasz Zabuski'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`
+  ]
+};
