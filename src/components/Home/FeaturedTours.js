d7ec20dbf38c382f4423569f1ec27e9db7da4a77
Łukasz <riraseth@gmail.com>
2020-03-18 22:08:09 +0100

    featured tours

---
+++ src/components/Home/FeaturedTours.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Tour from '../Tours/Tour';
+import { graphql, useStaticQuery } from 'gatsby';
+import StyledTitle from '../StyledTitle';
+import styles from '../../css/items.module.css';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+const getTours = graphql`
+  query {
+    featuredTours: allContentfulTour(filter: { featured: { eq: true } }) {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const FeaturedTours = () => {
+  // const {
+  //   featuredTours: { edges }
+  // } = useStaticQuery(getTours);
+  // const tours = edges;
+  const data = useStaticQuery(getTours);
+  const tours = data.featuredTours.edges;
+
+  return (
+    <section className={styles.tours}>
+      <StyledTitle title="featured" subtitle="tours"></StyledTitle>
+      <div className={styles.center}>
+        {tours.map(({ node }) => {
+          return <Tour key={node.contentful_id} {...node}></Tour>;
+        })}
+      </div>
+      <AniLink fade to="/tours" className="btn-primary">
+        all tours
+      </AniLink>
+    </section>
+  );
+};
+
+export default FeaturedTours;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Banner from '../components/Banner';
 import About from '../components/Home/About';
 import Services from '../components/Home/Services';
 import StyledHero from '../components/StyledHero';
+import FeaturedTours from '../components/Home/FeaturedTours';
 
 //with export we can skip useStaticQuery
 export const query = graphql`
@@ -36,6 +37,7 @@ export default ({ data }) => {
       </StyledHero>
       <About></About>
       <Services></Services>
+      <FeaturedTours></FeaturedTours>
     </Layout>
   );
 };
