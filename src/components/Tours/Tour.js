f6ccff688d2cc97bf86fa10e90ccf6e9eddaca0d
Łukasz <riraseth@gmail.com>
2020-03-18 22:21:20 +0100

    Update Tour.js

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -1,7 +1,37 @@
 import React from 'react';
-
-const Tour = () => {
-  return <div>hello from tour</div>;
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import Image from 'gatsby-image';
+import styles from '../../css/tour.module.css';
+import { FaMap } from 'react-icons/fa';
+const Tour = ({ name, price, country, days, slug, images }) => {
+  let mainImage = images[0].fluid;
+  return (
+    <article className={styles.tour}>
+      <div className={styles.imgContainer}>
+        <Image
+          fluid={mainImage}
+          className={styles.img}
+          alt="single tour"
+        ></Image>
+        <AniLink fade to={`tours/${slug}`} className={styles.link}>
+          details
+        </AniLink>
+      </div>
+      <div className={styles.footer}>
+        <h3>{name}</h3>
+        <div className={styles.info}>
+          <h4 className={styles.country}>
+            <FaMap className={styles.icon}></FaMap>
+            {country}
+          </h4>
+          <div className={styles.details}>
+            <h6>{days} days</h6>
+            <h6>from ${price}</h6>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Tour;
