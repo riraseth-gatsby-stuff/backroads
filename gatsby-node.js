a1bdf40c7e5ebb0fd128980244110ac65e13b65d
Łukasz Zabuski <riraseth@gmail.com>
2020-03-19 23:13:21 +0100

    tour template

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,29 @@ exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
     };
   }
 };
+
+const path = require(`path`);
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `);
+  data.tours.edges.forEach(({ node }) => {
+    createPage({
+      path: `tours/${node.slug}`,
+      component: path.resolve(`./src/templates/tour-template.js`),
+      context: {
+        slug: node.slug
+      }
+    });
+  });
+};
---
+++ src/templates/tour-template.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Template = () => {
+  return <div>hello from template</div>;
+};
+
+export default Template;
