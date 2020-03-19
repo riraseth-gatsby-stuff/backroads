50c4fb9ea4c1a41b3e567ee74b7fa0c3dc62620c
Łukasz Zabuski <riraseth@gmail.com>
2020-03-19 22:22:04 +0100

    default images

--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -4,9 +4,24 @@ import Image from 'gatsby-image';
 import styles from '../../css/tour.module.css';
 import { FaMap } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const getImage = graphql`
+  query {
+    file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`;
 
 const Tour = ({ name, price, country, days, slug, images }) => {
-  let mainImage = images[0].fluid;
+  const data = useStaticQuery(getImage);
+  const img = data.file.childImageSharp.fluid;
+  let mainImage = images[0].fluid ? images[0].fluid : img;
   return (
     <article className={styles.tour}>
       <div className={styles.imgContainer}>
