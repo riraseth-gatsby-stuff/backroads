b5c87699e86103f7b1e19ee6ce27881e939b33f6
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 22:05:22 +0100

    blogs template

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import styles from '../css/blog.module.css';
+import BlogCard from '../components/Blog/BlogCard';
+import StyledTitle from '../components/StyledTitle';
+const Blog = () => {
+  return <Layout></Layout>;
+};
+
+export default Blog;
