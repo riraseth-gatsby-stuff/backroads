b4ca25309fed0f8ad7860ebeb9192e64f53bf26e
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 20:50:49 +0100

    rich text images

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,18 +3,39 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import styles from '../css/single-blog.module.css';
 import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 const Blog = ({ data }) => {
   const {
     title,
     published,
     text: { json }
   } = data.post;
+  const options = {
+    renderNode: {
+      'embedded-asset-block': node => {
+        return (
+          <div className="rich">
+            <h3>this is awesome image</h3>
+            <img
+              src={node.data.target.fields.file['en-US'].url}
+              alt=""
+              width="400"
+            />
+            <p>Images provided by jane doe</p>
+          </div>
+        );
+      }
+    }
+  };
   return (
     <Layout>
       <section className={styles.blog}>
         <div className={styles.center}>
           <h1>{title}</h1>
           <h4>published at: {published}</h4>
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
           <AniLink fade to="/blog" className="btn-primary">
             all posts
           </AniLink>
