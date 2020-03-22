541cda41f8bfb2352a70cdda4411bf7c6d904368
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 16:22:29 +0100

    Update Day.js

--- src/components/SingleTour/Day.js
+++ src/components/SingleTour/Day.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
+import styles from '../../css/day.module.css';
+const Day = ({ day, info }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const Day = () => {
-  return <div>this is day 1</div>;
+  const handleClick = () => {
+    setIsOpen(isOpen => !isOpen);
+  };
+
+  return (
+    <article className={styles.day}>
+      <h4>
+        {day}
+        <button className={styles.btn} onClick={handleClick}>
+          <FaAngleDown></FaAngleDown>
+        </button>
+      </h4>
+      {isOpen && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Day;
