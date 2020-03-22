6d09d1f6c6d2abbfb6b1b9009cc09be765de15d0
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 22:28:16 +0100

    Update blog-list-template.js

--- src/templates/blog-list-template.js
+++ src/templates/blog-list-template.js
@@ -5,8 +5,45 @@ import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import styles from '../css/blog.module.css';
 import BlogCard from '../components/Blog/BlogCard';
 import StyledTitle from '../components/StyledTitle';
-const Blog = () => {
-  return <Layout></Layout>;
+// we're using props because we're passing numPages and currentPage from createPage in gatsby-node.js
+const Blog = props => {
+  const { data } = props;
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <StyledTitle title="latest" subtitle="posts"></StyledTitle>
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.id} {...node}></BlogCard>;
+          })}
+        </div>
+      </section>
+    </Layout>
+  );
 };
+
+export const query = graphql`
+  query getPosts($skip: Int, $limit: Int) {
+    posts: allContentfulPost(
+      skip: $skip
+      limit: $limit
+      sort: { fields: published, order: DESC }
+    ) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Blog;
