fd170c16f849feac1c839d1f8e114a67f4768d5b
Łukasz Zabuski <riraseth@gmail.com>
2020-03-22 22:21:26 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,4 +49,22 @@ exports.createPages = async ({ actions, graphql }) => {
       }
     });
   });
+  // amount of posts
+  const posts = data.posts.edges;
+  // posts per page
+  const postsPerPage = 5;
+  // how many pages
+  const numPages = Math.ceil(posts.length / postsPerPage);
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `blogs` : `blogs/${i + 1}`,
+      component: path.resolve(`./src/templates/blog-list-template.js`),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1
+      }
+    });
+  });
 };
