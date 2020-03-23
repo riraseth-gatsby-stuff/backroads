e142008902f0bafc663a990759853fa1730e8e5a
Łukasz Zabuski <riraseth@gmail.com>
2020-03-23 22:55:09 +0100

    facebook card added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -25,14 +25,23 @@ const SEO = ({ title, description }) => {
 
   return (
     <Helmet htmlAttributes={{ lang: 'en' }} title={`${title} | ${siteTitle}`}>
-      <meta name="description" content={description || siteDesc}></meta>
-      <meta name="image" content={image}></meta>
+      <meta name="description" content={description || siteDesc} />
+      <meta name="image" content={image} />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      {/* facebook cards */}
+
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
     </Helmet>
   );
 };
